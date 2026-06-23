70f865bb6e15c552e1f8f9e66d7691f2286cb6fc
Eric Nam <audgh3364@gmail.com>
2026-06-23 18:20:24 +0900

    [KAN-46] Project Init

---
+++ src/test/java/com/sparta/contractmanagement/ContractManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.contractmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContractManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
